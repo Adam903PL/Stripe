f4405bb26af86d92bc549f060588be4ce0a1950d
Adam Pukaluk <pukaluk.adam505@gmail.com>
2025-10-29 20:32:08 +0100

    Initialize project without secrets

---
+++ src/server.js
@@ -0,0 +1,49 @@
+
+import express from "express";
+import Stripe from "stripe";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+
+const app = express();
+const PORT = 4000;
+
+dotenv.config();
+
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+
+if (!stripeSecretKey) {
+  throw new Error("Missing STRIPE_SECRET_KEY environment variable");
+}
+
+const stripe = new Stripe(stripeSecretKey);
+
+app.use(bodyParser.json());
+app.use(express.static("public")); // 
+
+app.post("/create-checkout-session", async (req, res) => {
+  try {
+    const { priceId } = req.body; 
+    console.log("📦 priceId:", priceId);
+
+    const session = await stripe.checkout.sessions.create({
+      mode: "subscription",
+      line_items: [
+        {
+          price: priceId, 
+          quantity: 1,
+        },
+      ],
+      success_url: "http://localhost:4000/success.html?session_id={CHECKOUT_SESSION_ID}",
+      cancel_url: "http://localhost:4000/cancel.html",
+      subscription_data: {
+      },
+    });
+
+    res.json({ url: session.url });
+  } catch (err) {
+    console.error("❌ Error creating checkout session:", err.message);
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.listen(PORT, () => console.log(`🚀 Server running at http://localhost:${PORT}`));
